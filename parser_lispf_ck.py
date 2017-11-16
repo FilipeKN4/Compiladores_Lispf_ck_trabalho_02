69ef8eb7728e1fd6cd764d2d652aa3fb07fcdd07
danielmarques28 <danielmarques7@hotmail.com>
2017-11-16 09:26:58 -0200

    Add lexer NUMBER, DO

--- parser_lispf_ck.py
+++ parser_lispf_ck.py
@@ -1,19 +1,29 @@
 import ox
 
 lexer = ox.make_lexer([
-    ('loop', r'loop'),
-    ('dec', r'dec'),
-    ('inc', r'inc'),
-    ('lpar', r'\('),
-    ('rpar', r'\)'),
-    ('right', r'right'),
-    ('left', r'left'),
-    ('print', r'print'),
-    ('read', r'read')
+	('NUMBER', r'[0-9]+'),
+	('DO', r'do'),
+	('ADD', r'add'),
+    ('LOOP', r'loop'),
+    ('DEC', r'dec'),
+    ('INC', r'inc'),
+    ('LPAR', r'\('),
+    ('RPAR', r'\)'),
+    ('RIGHT', r'right'),
+    ('LEFT', r'left'),
+    ('PRINT', r'print'),
+    ('READ', r'read')
 ])
 
-tokens = ['dec', 'inc', 'right', 'left', 'print']
+tokens = ['NUMBER', 'DO', 'ADD', 'LOOP, ''DEC', 'INC', 'LPAR', 'RPAR', 'LEFT', 'RIGHT', 'PRINT', 'READ']
+
+parser = ox.make_parser([
+	('term : LPAR DO ADD NUMBER RPAR', str),
+], tokens)
 
 source = input('lispf_ck: ')
 tokens = lexer(source)
 print('tokens: ', tokens)
+
+#result = parser(tokens)
+#print('result: ', result)
