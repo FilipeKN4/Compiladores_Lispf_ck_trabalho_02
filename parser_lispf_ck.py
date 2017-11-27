f0053cc5938d98048cd76283006b3e9977e8b6b8
FilipeKN4 <filipekn4@gmail.com>
2017-11-27 16:34:20 -0200

    Updates lexer and adds teste1.lf

--- parser_lispf_ck.py
+++ parser_lispf_ck.py
@@ -1,19 +1,54 @@
 import ox
+import click
 
 lexer = ox.make_lexer([
-    ('loop', r'loop'),
-    ('dec', r'dec'),
-    ('inc', r'inc'),
-    ('lpar', r'\('),
-    ('rpar', r'\)'),
-    ('right', r'right'),
-    ('left', r'left'),
-    ('print', r'print'),
-    ('read', r'read')
+    ('LOOP', r'loop'),
+    ('DEC', r'dec'),
+    ('INC', r'inc'),
+    ('LPAR', r'\('),
+    ('RPAR', r'\)'),
+    ('RIGHT', r'right'),
+    ('LEFT', r'left'),
+    ('PRINT', r'print'),
+    ('READ', r'read'),
+    ('DO', r'do'),
+    ('DO_AFTER', r'do-after'),
+    ('DO_BEFORE', r'do-before'),
+    ('ADD', r'add'),
+    ('NUMBER', r'[0-9]+'),
+    ('COMMENT', r';[^\n]*'),
+    ('BREAK_LINE', r'\n'),
+    ('SPACE', r'\s')
 ])
 
-tokens = ['dec', 'inc', 'right', 'left', 'print']
+tokens = ['DEC',
+          'INC',
+          'RIGHT',
+          'LEFT',
+          'PRINT',
+          'LOOP',
+          'LPAR',
+          'RPAR',
+          'RIGHT',
+          'LEFT',
+          'PRINT',
+          'READ',
+          'DO',
+          'DO_AFTER',
+          'DO_BEFORE',
+          'ADD',
+          'NUMBER',
+          'COMMENT',
+          'BREAK_LINE',
+          'SPACE']
 
-source = input('lispf_ck: ')
-tokens = lexer(source)
-print('tokens: ', tokens)
+@click.command()
+@click.argument('source', type=click.File('r'))
+
+def make_ast(source):
+    source_code = source.read()
+    tokens = lexer(source_code)
+    print('tokens: ', tokens)
+
+if __name__ == '__main__':
+    make_ast()
